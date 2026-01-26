b65a7f4c919924e6c5eb6fbf03628402b6721724
Stork <marystork@yandex.ru>
2026-01-26 19:07:13 +0300

    docs: improve DataLoader docstrings and typing

--- src/loader.py
+++ src/loader.py
@@ -7,23 +7,46 @@ from pathlib import Path
 
 class DataLoader:
     """
-    Responsible for loading dataset from CSV.
+    Loads dataset from a CSV file.
+
+    Attributes:
+        logger : Logger
+            Logger instance for logging messages and saving logs
+        validator : Validator
+            Validator instance for validating input data
+        path : Path
+            Path to CSV file
+        df : pd.DataFrame or None
+            Loaded DataFrame (None until loaded)
     """
 
     def __init__(self, path: Path = Path('data/raw/heart-diseases.csv')):
-        self.path: Path = path.resolve()
+        """
+        Initializes DataLoader.
 
+        Parameters:
+            path : Path, optional
+                Path to the CSV file (default 'data/raw/heart-diseases.csv')
+        """
+
+        # Component initialization
+        self.logger: Logger = get_logger()
         self.validator = Validator()
+
+        # Variables initialization
+        self.path: Path = path.resolve()
         self.validator.check_type_path(self.path)
         self.validator.check_file_exists(self.path)
-
         self.df: pd.DataFrame | None = None
-        self.logger: Logger = get_logger()
 
 
     def load(self) -> pd.DataFrame:
         """
         Load CSV file into pandas DataFrame.
+
+        Returns:
+            pd.DataFrame
+                Loaded DataFrame
         """
 
         self.df = pd.read_csv(self.path)
