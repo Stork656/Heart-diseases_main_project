5cc94fc980c02c945d4bed11664cc60a84fbba69
Stork <marystork@yandex.ru>
2026-01-28 18:03:55 +0300

    docs: improve docstrings and typing

--- src/preprocessing/advanced.py
+++ src/preprocessing/advanced.py
@@ -7,19 +7,29 @@ from sklearn.ensemble import IsolationForest
 
 class AdvancedPreprocessor(BasePreprocessor):
     """
-
+    AdvancedPreprocessor is a subclass of BasePreprocessor
+    Performs a sophisticated steps of preprocessing pipeline:
+    - Applies encoded by frequency
+    - Missing values are imputed using KNNImputer
+    - Feature scaling with RobustScaler
+    - Filters outliers using IsolationForest
     """
-
-
-    def __init__(self, df: pd.DataFrame, target: str = 'HeartDisease'):
+    def __init__(self, df: pd.DataFrame, target: str = 'HeartDisease') -> None:
+        """
+        Initializes StandardPreprocessor
+        Parameters:
+            df : pd.DataFrame
+                Input DataFrame to preprocess from parent class
+            target : str, optional
+                Target column name from parent class (default is 'HeartDisease')
+        """
         super().__init__(df, target)
 
 
     def encoding(self) -> None:
         """
-
+        Applies encoded by frequency
         """
-
         encoding_data = self.categorical_cols + self.binary_cols
         for column in encoding_data:
             freq = self.df[column].value_counts(normalize=True)
@@ -28,29 +38,31 @@ class AdvancedPreprocessor(BasePreprocessor):
 
     def remove_missing(self) -> None:
         """
-        Remove the missing values from dataframe with KNNImputer
+        Missing values are imputed using KNNImputer
         (number of neighbors = 5)
         """
         self.df = self.df.dropna(subset=[self.target])
 
-        if super().remove_missing():
+        if super().check_missing():
             imputer = KNNImputer(n_neighbors=5)
-            self.df.loc[:, :] = imputer.fit_transform(self.df)
+            self.df.loc[:, :] = pd.DataFrame(
+                imputer.fit_transform(self.df),
+                columns = self.df.columns,
+                index = self.df.index
+            )
 
 
     def scaling(self) -> None:
         """
         Scaling of numerical features with RobustScaler
         """
-
         scaler = RobustScaler()
-        self.df[self.numeric_cols] = self.df[self.numeric_cols].astype(float)
-        self.df.loc[:, self.numeric_cols] = scaler.fit_transform(self.df.loc[:, self.numeric_cols])
+        self.df[self.numeric_cols] = scaler.fit_transform(self.df[self.numeric_cols])
 
 
     def remove_outliers(self) -> None:
         """
-
+        Filters outliers using IsolationForest
         """
         iso = IsolationForest(contamination='auto', random_state=42)
         mask = iso.fit_predict(self.df[self.numeric_cols])
@@ -59,7 +71,7 @@ class AdvancedPreprocessor(BasePreprocessor):
 
     def run(self) -> None:
         """
-        Run full advansed preprocessing pipeline
+        Run full advanсed preprocessing pipeline
         """
 
         self.remove_duplicates()
